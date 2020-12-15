97d84db6b2501f934454ea384815baeb24d91464
Alexander Sherwin <alx.sherwin@gmail.com>
2020-12-15 11:27:49 -0700

    commit tree 4ad7380e170cd71b72824015151b3f44dc75a0f3

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,52 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  the_winner = data[season].find do |contestant|
+    contestant["status"] == "Winner"
+  end
+the_winner["name"].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestants = get_all_contestants(data)
+  matching_contestant = find_contestant_by_attribute contestants, "occupation", occupation
+  matching_contestant["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  get_all_contestants(data).count do |contestant|
+    contestant["hometown"] == hometown
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  get_all_contestants(data).find do |contestant|
+    
+    if hometown == contestant["hometown"]
+      return contestant["occupation"]
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+
+  data[season].select do |contestant|
+  age_array << contestant["age"].to_i 
+  end
+  average_age = age_array.inject do|sum, el| 
+    (sum + el).to_f / age_array.size
+  end
+
+  average_age
+end
+
+def find_contestant_by_attribute contestants, key, value
+  contestants.find do |contestant|
+    contestant[key] == value
+  end
+end
+
+def get_all_contestants data
+  data.values.flatten
 end
